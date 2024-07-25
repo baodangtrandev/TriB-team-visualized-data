d3081afb0531d5e49338e54f540bd76d9f7f2818
Tran Dang Bao <trandangbao2004@gmail.com>
2024-07-25 16:40:13 +0700

    add read xlsx file

--- app.py
+++ app.py
@@ -11,11 +11,28 @@ st.set_page_config(
 
 st.title("TriB Visualization App")
 
+selection = st.selectbox("Select your file type", ("*.csv", "*.xlsx", "*.json"))
+
 uploaded_file = st.file_uploader("Choose a file")
 
+
+
+
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, encoding='latin1')
-    pyg_app = StreamlitRenderer(df)
-    pyg_app.explorer()
+    df = None
+    if selection == "*.csv":
+        df = pd.read_csv(uploaded_file, encoding='latin1')
+        pyg_app = StreamlitRenderer(df)
+        pyg_app.explorer()
+    elif selection == "*.xlsx":
+        df = pd.read_excel(uploaded_file)
+        st.write("load successfully")
+        pyg_app = StreamlitRenderer(df)
+        pyg_app.explorer()
+    elif selection == "*.json":
+        df = pd.read_json(uploaded_file)
+        pyg_app = StreamlitRenderer(df)
+        pyg_app.explorer()
+    
 
 
